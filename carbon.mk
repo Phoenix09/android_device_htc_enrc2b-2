4424dcbcc67119886dec9fcb7348ca5100cd33ae
DRW9 <Phoenix09@users.noreply.github.com>
2015-09-23 03:49:12 +0100

    Carbon

--- carbon.mk
+++ carbon.mk
@@ -5,18 +5,20 @@ PRODUCT_RELEASE_NAME := enrc2b
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/enrc2b/full_enrc2b.mk)
 
+# languages
+PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
+
+PRODUCT_PACKAGES += Tegra3Parts
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := enrc2b
-PRODUCT_NAME := cm_enrc2b
+PRODUCT_NAME := carbon_enrc2b
 PRODUCT_BRAND := htc_europe
 PRODUCT_MODEL := One X+
 PRODUCT_MANUFACTURER := HTC
